976d66ddad4ce481fb89fe38ed7cb4243b74d4f2
Vaibhav Tale <vaibhavtale20@gmail.com>
2026-05-30 15:08:53 +0530

    Add comments and video import to firebase_options.dart
    Added comments and an import statement for video.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -4,6 +4,11 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
+
+//Adding This comments
+
+import 'package:flutter/foundation/video.dart'
+
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
 /// Example:
