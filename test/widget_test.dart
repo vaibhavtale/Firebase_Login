895f9124f220a54e749e72459ef815d93cb512fa
Vaibhav Tale <vaibhavtale20@gmail.com>
2026-05-30 15:49:49 +0530

    Add camera.dart import to widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_fire/main.dart';
+import 'package:flutter_fire/camera.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
